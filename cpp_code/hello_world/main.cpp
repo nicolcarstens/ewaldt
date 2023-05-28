419dbbdc99db0c8a7689ef3752de980909a090e1
Nicol Carstens <nicolcarstens@gmail.com>
2023-05-28 17:58:04 +0200

    add cpp main

---
+++ cpp_code/hello_world/main.cpp
@@ -0,0 +1,16 @@
+// C++ program to display "Hello World"
+// $ g++ main.cpp -Wall
+ 
+// Header file for input output functions
+#include <iostream>
+
+using namespace std;
+ 
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World!\n";
+ 
+    return 0;
+}
